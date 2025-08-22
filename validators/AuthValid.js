a3a1214066278354b788ba370ce4fd80e37eb4fa
manishkumar832 <bodakuntimanishkumar@gmail.com>
2025-08-22 12:28:42 +0530

    project

--- validators/AuthValid.js
+++ validators/AuthValid.js
@@ -2,7 +2,7 @@ const {body,validationResult, header}=require("express-validator")
 
 exports.SignupValidator=[
     body("name").isLength({min:3,max:15}).trim().isString().withMessage("inavlid name"),
-    body("email").isAlphanumeric().trim().isEmail().withMessage("inavlid name"),
+    body("email").isString().trim().isEmail().withMessage("inavlid name"),
     body("password").isLength({min:3,max:15}).trim().isAlphanumeric().withMessage("inavlid name"),
     body("username").isLength({min:3,max:15}).trim().isString().withMessage("inavlid name"),
     (req,res,next)=>{
@@ -22,7 +22,7 @@ exports.SignupValidator=[
 
 
 exports.LoginValidator=[
-   body("email").isAlphanumeric().trim().isEmail().withMessage("inavlid name"),
+   body("email").isString().trim().isEmail().withMessage("inavlid name"),
     body("password").isLength({min:3,max:15}).trim().withMessage("inavlid name"),
      (req,res,next)=>{
         const result=validationResult(req)
