45a88abf867df5958c8102dd47cd69ecd867203f
manishkumar832 <bodakuntimanishkumar@gmail.com>
2025-08-25 12:34:51 +0530

    updated

--- controllers/AuthControll.js
+++ controllers/AuthControll.js
@@ -11,7 +11,7 @@ const signupController=async(req,res,next)=>{
         email:email,
         username:username,
         password:hashpass,
-        role:role || "jobseeker"
+        role:role || "employee"
     })
     const SaveData=await user.save()
     res.send(SaveData)
