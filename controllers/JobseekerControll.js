bbd41c1a4559bcb96dd965491835537ac2c83052
manishkumar832 <bodakuntimanishkumar@gmail.com>
2025-09-14 23:25:21 +0530

    validators update

--- controllers/JobseekerControll.js
+++ controllers/JobseekerControll.js
@@ -28,55 +28,48 @@ const ShowJobsById = async (req, res, next) => {
 
 
 // applications Apis
-const ApplyJob = async (req, res, next) => {
-  try {
-    const { jobId, name, email, phone, skills, education } = req.body
+const ApplyJob=async(req,res,next)=>{
+   try {
+    const {jobId, name, email, phone, skills, education}=req.body
     const resume = req.files?.resume?.[0];
     const coverLetter = req.files?.coverLetter?.[0];
+    
+    const resumeUrl=await uploadCloudinary(resume.path)
+    const coverurl=await uploadCloudinary(coverLetter.path)
+      fs.unlink(resume.path, (err) => {
+  if (err) {
+    console.error("Failed to delete resume file:", err);
+  }
+});
 
-
-    const resumeUrl = await uploadCloudinary(resume.path)
-    const coverurl = await uploadCloudinary(coverLetter.path)
-    fs.unlink(resume.path, (err) => {
-      if (err) {
-        console.error("Failed to delete resume file:", err);
-      }
-    });
-
-    fs.unlink(coverLetter.path, (err) => {
-      if (err) {
-        console.error("Failed to delete cover letter file:", err);
-      }
-    });
-
-    const jobs = await Jobs.findById(jobId)
-    if (!jobs) {
+fs.unlink(coverLetter.path, (err) => {
+  if (err) {
+    console.error("Failed to delete cover letter file:", err);
+  }
+});
+  
+    const jobs=await Jobs.findById(jobId)
+    if(!jobs){
       return res.status(404).send("job Not found")
 
     }
-
-
     const alreadyApplied = await Application.findOne({ jobId, applicant: req.userId._id });
 
-    if (alreadyApplied) {
+    if(alreadyApplied){
       return res.status(400).send("You have Already Applied")
     }
 
-    const applyDetails = new Application({
-      job: jobId,
-      applicant: req.userId,
-      name, email, phone, skills, education, resume: resumeUrl, coverLetter: coverurl
+    const applyDetails=new Application({
+      job:jobId,
+      applicant:req.userId,
+      name,email,phone,skills,education,resume:resumeUrl,coverLetter:coverurl
     })
 
     await applyDetails.save()
-
-    const mail = await sendingMails(email)
-    console.log("passed mail", mail)
-
-    res.status(200).json({ message: "Applied Successfully", Data: applyDetails })
-  } catch (error) {
-    next({ statusCode: 400, err: error.message })
-  }
+    res.status(200).json({message:"Applied Successfully",Data:applyDetails})
+   } catch (error) {
+    next({statusCode:400,err:error.message})
+   }
 }
 const myApplications = async (req, res, next) => {
   try {
